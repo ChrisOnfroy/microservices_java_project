d1e7676affde7f90b6386b112af4e5df9ac29893
Cristian Morales Avendano <cmoralesavendano6@gmail.com>
2024-09-08 06:21:00 -0500

    inicio nuevo de proyecto

---
+++ taks_microservices/src/main/java/com/example/taks_microservices/TaksMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.taks_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaksMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaksMicroservicesApplication.class, args);
+	}
+
+}
